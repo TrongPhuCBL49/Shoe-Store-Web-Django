b25624d7130956d11771a20a5e580a4998a80966
Augustino Nguyen <trongphu.cbl49@gmail.com>
2019-11-07 13:31:54 +0700

    Add template and load base html

--- ShoeStore/settings.py
+++ ShoeStore/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'product',
     'order',
     'user',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -58,7 +59,7 @@ ROOT_URLCONF = 'ShoeStore.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,3 +125,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'user.CustomerUser'
+
+STATICFILES_DIR = [
+    os.path.join(BASE_DIR, 'static'),
+]
