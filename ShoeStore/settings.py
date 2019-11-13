74ea89e17370b4cd3d50cb0b1acdfefb81cabe3e
Augustino Nguyen <trongphu.cbl49@gmail.com>
2019-11-13 15:07:13 +0700

    View list product

--- ShoeStore/settings.py
+++ ShoeStore/settings.py
@@ -122,10 +122,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
-
 AUTH_USER_MODEL = 'user.CustomerUser'
+
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
    os.path.join(BASE_DIR, "static"),
 ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- core/views.py
+++ core/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.views import View
+from product.models import Product
 # Create your views here.
 
-class HomeView(View):
-    def get(self, request):
-        return render(request, 'homepage/index.html')
+# class HomeView(View):
+#     def get(self, request):
+#         Products = {'Products': Product.objects.all()}
+#         return render(request, 'homepage/index.html', Products)
+
+def Home(request):
+    Products = {'Products': Product.objects.all()}
+    return render(request, 'homepage/index.html', Products) 
+   
