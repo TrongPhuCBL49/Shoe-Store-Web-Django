41c3fbde5184778259e04b366b0c552f690c2d10
Augustino Nguyen <trongphu.cbl49@gmail.com>
2019-11-22 15:31:59 +0700

    Create category.html and split to blocks

--- product/urls.py
+++ product/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ProductDetailView
+from .views import ProductDetailView, CategoryView
 
 app_name = 'product'
 
 urlpatterns = [
     path('<int:pk>/', ProductDetailView, name='product-detail'),
+    path('category/', CategoryView.as_view(), name='category'),
 ]
--- product/views.py
+++ product/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, HttpResponse
 from django.views import View
 from .models import Product
 # Create your views here.
@@ -6,3 +6,7 @@ from .models import Product
 def ProductDetailView(request, pk):
     single_product = {"Product": get_object_or_404(Product, pk=pk)}
     return render(request, 'product/product-details.html', single_product)
+
+class CategoryView(View):
+    def get(self, request):
+        return render(request, 'product/category.html')
