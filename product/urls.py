de4d203615a3c1c2976767c1031896d84ceb2151
Augustino Nguyen <trongphu.cbl49@gmail.com>
2019-11-23 20:14:40 +0700

    Mapping fake url :)) and show categories

--- product/urls.py
+++ product/urls.py
@@ -4,6 +4,6 @@ from .views import ProductDetailView, CategoryView
 app_name = 'product'
 
 urlpatterns = [
-    path('<int:pk>/', ProductDetailView, name='product-detail'),
-    path('category/', CategoryView.as_view(), name='category'),
+    path('<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
+    path('category/<slug:slug>/<int:pk>/', CategoryView.as_view(), name='category'),
 ]
--- product/views.py
+++ product/views.py
@@ -1,12 +1,17 @@
-from django.shortcuts import render, get_object_or_404, HttpResponse
+from django.shortcuts import render, get_object_or_404, HttpResponse, get_list_or_404
 from django.views import View
-from .models import Product
+from .models import Product, Category
 # Create your views here.
 
-def ProductDetailView(request, pk):
-    single_product = {"Product": get_object_or_404(Product, pk=pk)}
-    return render(request, 'product/product-details.html', single_product)
+class ProductDetailView(View):
+    def get(self, request, pk):
+        single_product = {"Product": get_object_or_404(Product, pk=pk)}
+        return render(request, 'product/product-details.html', single_product)
 
 class CategoryView(View):
-    def get(self, request):
-        return render(request, 'product/category.html')
+    def get(self, request, ):
+        categories = {"Categories": Category.objects.all()}
+        return render(request, 'product/category.html', categories)
+
+    def post(self, request):
+        products_category = get_list_or_404(Product, category=pk)
