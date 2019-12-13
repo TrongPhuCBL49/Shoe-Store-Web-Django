e1b9d8a3b42978d4c8590a9e840dbda44234d056
Augustino Nguyen <trongphu.cbl49@gmail.com>
2019-12-13 19:47:30 +0700

    Add user/login.html

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import RegisterView
+from .views import RegisterView, LoginView
 
-app_name = 'product'
+app_name = 'user'
 
 urlpatterns = [
-    path('', RegisterView.as_view(), name='register'),
+    path('register/', RegisterView, name='register'),
+    path('login/', LoginView, name='login'),
 ]
--- user/views.py
+++ user/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.views import View
 # Create your views here.
 
-class RegisterView(View):
-    def get(self, request):
-        return render(request, 'user/register.html')
+def Register(request):
+    return render(request, 'user/register.html')
+
+def LoginView(request):
+    return render(request, 'user/login.html')
+
